5bf27fa207cfb12bf418a4b457bb56a95b41751e
SenneVanderAuwera <senne.vanderauwera@telenet.be>
2020-02-23 15:36:55 +0100

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1,10 +1,42 @@
 <!doctype html>
 <html>
 	<head>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+	
+		<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+		
+		<head>
+		  <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+		  <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+		</head>
 		<meta charset="utf-8">
 		<title>Projecting Web-App</title>
 	</head>
 	<body>
-		Hello world
+		<!-- ###NAVBAR### -->
+		<nav class="navbar bg-dark">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="dashboard.php">Link 1</a>
+				</li>			
+				<li class="nav-item">
+					<a class="nav-link" href="dashboard.php">Link 2</a>
+				</li>
+			</ul>	
+		</nav>
+		
+		<div class="mdc-text-field mdc-text-field--outlined">
+		  <input class="mdc-text-field__input" id="text-field-hero-input">
+		  <div class="mdc-notched-outline mdc-notched-outline--no-label">
+			<div class="mdc-notched-outline__leading"></div>
+			<div class="mdc-notched-outline__notch">
+			  <label for="text-field-hero-input" class="mdc-floating-label">Label</label>
+			</div>
+			<div class="mdc-notched-outline__trailing"></div>
+		  </div>
+		</div>		
+		
 	</body>
 </html>
